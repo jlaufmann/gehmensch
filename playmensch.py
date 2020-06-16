c451fa470a6ad3b52d207d0c5eebb47b90b96a4d
Jimbo Laufmann <jimbo@e.email>
2020-06-16 22:14:47 +0200

    playmensch.py - .m3u playlist generator

---
+++ playmensch.py
@@ -0,0 +1,99 @@
+'''
+
+This script constructs an .M3U playlist file
+You may wish to first run mediamensch.py over the audio files you want to add
+to the playlist to ensure the relevant information is included in the .M3U
+files.
+
+playmensch will add all files (not folders) to the playlist if they have a
+suitable extension.
+
+playmensch should be run from where the .m3u files are needed
+
+
+#EXTM3U
+
+#EXTINF:105, Example artist - Example title
+C:\Files\My Music\Example.mp3
+
+#EXTINF:321, Example Artist2 - Example title2
+C:\Files\My Music\Favorites\Example2.ogg
+
+'''
+
+import os
+from mutagen.easyid3 import EasyID3
+from mutagen.mp3 import MP3
+
+def findext(fname):
+	if fname.find('.') == -1:
+		fext = ''
+	else:
+		for i in range(len(fname)):
+			if fname[i] == '.':
+				fext = fname[i:]
+	return fext
+
+
+def startextline(text):
+	stars1 = '*' * ((80-len(text))//2)
+	stars2 = '*' * (80-len(stars1) - len(text))
+	print(f"{stars1}{text}{stars2}")
+
+# files with these extensions will be added to the playlist:
+exts = ['.mp3']
+
+os.system('cls' if os.name == 'nt' else 'clear')  # clearing the screen
+
+print(f"{'*'*80}")
+startextline(' .M3U PLAYLIST GENERATOR v1.0 ')
+startextline(' BY JIM PINCINI ')
+print(f"{'*'*80}")
+
+print(f"\nCurrent Folder: {os.getcwd()}")
+fol_in = input(f"\nEnter relative path of folder containing files to add to playlist ('.' for current folder):\n ")
+fol_in = fol_in.rstrip('/')
+#fol_in = 'podcasts/Under The Skin with Russell Brand'
+
+if fol_in != '.':
+	print(f"\nFor Folder: {str(os.getcwd()) + '/' + fol_in + '/'}")
+else:
+	print(f"\nFor Folder: {str(os.getcwd()) + '/'}")
+
+filelistf = []
+
+print(f"\nAudio File list: ")
+
+for filename in sorted(os.listdir(fol_in), key=str.lower):
+	if os.path.isfile(os.path.join(fol_in, filename)) and findext(filename).lower() in exts:
+		try:
+			audio = EasyID3(os.path.join(fol_in, filename))
+		except:
+			pass
+		else:
+			print(f"{filename}")
+			filelistf.append(os.path.relpath(os.path.join(fol_in, filename)))
+
+if len(filelistf) == 0:
+	print(f"NO AUDIO FILES FOUND!")
+	quit()
+
+plistf = input(f"\nWhat is the playlist name?: ") + '.m3u'
+
+with open(plistf, 'w') as fout:
+	fout.write(f"#EXTM3U\n\n")
+
+	for i in range(len(filelistf)):
+		filenamef = filelistf[i]
+		audio = EasyID3(filenamef)
+		artist = str(audio['artist']).strip('[]').strip('\'')
+		title = str(audio['title']).strip('[]').strip('\'')
+		try:
+			length = int(MP3(filenamef).info.length)
+		except:
+			length = -1
+
+		fout.write(f"#EXTINF:{length}, {artist} - {title}")
+		fout.write(f"\n{filenamef}\n\n")
+
+print(f"Playlist filename: {plistf}\n")
