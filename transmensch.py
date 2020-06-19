85731f70080caa31fb7df20dedb956a73a6612e6
Jimbo Laufmann <jimbo@e.email>
2020-06-19 12:59:08 +0200

    initial commit of transmensch.py

---
+++ transmensch.py
@@ -0,0 +1,8 @@
+'''
+It is planned for this script to be able to copy (pre-prepared) audio files
+and/or playlists files and/or folders to the sony walkman.
+
+It could also be used to delete folders/files/playlists from the walkman.
+
+
+'''
